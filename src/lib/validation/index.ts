f82d14e24da1c93a9dacd38e9ad6e1edd0c03014
aimen114 <aimenzafar095@gmail.com>
2023-12-26 13:05:04 -0800

    completed signup form

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,7 +1,13 @@
 import * as z from "zod";
 export const SignupValidation = z.object({
-  name: z.string().min(2, { message: "Too short" }),
-  username: z.string().min(2, { message: "Too short" }),
+  name: z
+    .string()
+    .min(2, { message: "Too short! name must be at least 2 characters" }),
+  username: z
+    .string()
+    .min(2, { message: "Too short! username must be at least 2 characters" }),
   email: z.string().email(),
-  password: z.string().min(8, { message: "Too short" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
 });
